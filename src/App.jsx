e8dcb93261ed41ef10a772855d7aea4cb06ba6f1
ghofrannateel <ghofranesam98@gmail.com>
2025-07-31 19:57:35 +0300

    task

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+
+import Footer  from "./components/footer/Footer.jsx"
+import Navbar from "./components/navbar/Navbar.jsx"
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/home/Home";
+import About from "./pages/about/About";
+import Create from "./pages/user/Create"; 
+export default function App() {
+  return (
+ <>
+ <Navbar />
+
+<Routes>
+  <Route path='/' element={<Home />}> </Route>
+    <Route path='/users' element={<Home />}> </Route>
+
+  <Route path='/add' element={<Create />}> </Route>
+
+  <Route path='/about' element={<About />}> </Route>
+
+</Routes>
+
+
+ <Footer />
+ 
+ 
+ </>
+  )
+}
